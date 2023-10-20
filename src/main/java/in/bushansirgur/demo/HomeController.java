2eb99b888f954126d6a0788f2dcf54105673391d
rahulknath <rahulnath995762@gmail.com>
2023-10-20 10:51:44 +0530

    latest

--- src/main/java/in/bushansirgur/demo/HomeController.java
+++ src/main/java/in/bushansirgur/demo/HomeController.java
@@ -1,7 +1,7 @@
 package in.bushansirgur.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HomeController {
@@ -9,5 +9,17 @@ public class HomeController {
 	@GetMapping({"/", "/home", "/status"})
 	public String getStatus() {
 		return "Application is up and running";
+	}
+	@PostMapping("/callback")
+	public String handleCallback(
+			@RequestParam(name = "token") String token,
+			@RequestParam(name = "hr_id") String hrId,
+			@RequestBody MultiValueMap<String, String> formData) {
+		// Handle the incoming data here
+		String jsonData = formData.getFirst("data"); // Extract data from the form data
+		System.out.println(jsonData);
+		// You can access 'token', 'hrId', and 'jsonData'
+		// Perform your processing and return a response if needed
+		return "Response"; // You can return a response message or object here
 	}
 }
